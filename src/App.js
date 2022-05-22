be7c3af4f2ae390e9177fe94da228bce53aae451
ZakyUp <andresjr195@hotmail.com>
2022-05-21 21:46:03 -0500

    breaking-bad

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const url = 'https://breakingbadapi.com/api/characters';
+  const [data, setData] = useState();
+
+  const fetchApi = async () => {
+    const res = await fetch(url);
+    const json = await res.json();
+    console.log(json);
+    setData(json)
+  }
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Breaking Bad</h1>
+      {
+        !data ? 'Cargando...' :
+        data.map((a, b) => {
+          return <div className='card'>
+                  <h2>{a.name}</h2>
+                  <img key={a.char_id} src={a.img} alt=''/>
+                  <p>Apodo: {a.nickname}</p>
+                  <p>Actor: {a.portrayed}</p>
+                  <p>Ocupacion: {a.occupation}</p>
+                </div>
+        })
+      }
     </div>
   );
 }
---
+++ src/components/episodios.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const episodios = () => {
+    const url = 'https://breakingbadapi.com/api/episodes';
+    
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default episodios
