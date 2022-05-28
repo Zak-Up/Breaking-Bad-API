2d6785895aaf02678677aaeeb7809329c0e02ad6
ZakyUp <andresjr195@hotmail.com>
2022-05-28 14:07:01 -0500

    finish

--- src/components/FetchApi.js
+++ src/components/FetchApi.js
@@ -1,9 +1,11 @@
 const FetchApi = async (changeDeath) => {
-    const res = await fetch('https://breakingbadapi.com/api/random-death',{ cache: "reload"});
+    const res = await fetch('https://breakingbadapi.com/api/random-death');
     const json = await res.json();
     
     changeDeath(json);
 };
 
 export default FetchApi;
+
+//,{ cache: "no-cache"}
 
